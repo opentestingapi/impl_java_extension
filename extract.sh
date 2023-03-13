3d36a2804025ca96f362c98c22242ac37775cd2b
Robert Diers <robertdiers@gmail.com>
2023-03-13 18:19:37 +0100

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,5 +1,5 @@
 # extract data from container
 rm -rf app
-container_id=$(docker create opentesting:latest)
+container_id=$(docker create opentesting:1.26)
 docker cp $container_id:/app .
 docker rm $container_id
