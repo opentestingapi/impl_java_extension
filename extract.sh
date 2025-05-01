7c33a7acea8eb5cb838530c934d14312ce3551cc
Robert Diers <robertdiers@gmail.com>
2025-05-01 16:07:01 +0200

    1.41

--- extract.sh
+++ extract.sh
@@ -1,5 +1,5 @@
 # extract data from container
 rm -rf app
-container_id=$(docker create ghcr.io/opentestingapi/opentesting:1.40)
+container_id=$(docker create ghcr.io/opentestingapi/opentesting:1.41)
 docker cp $container_id:/app .
 docker rm $container_id
--- extractPodman.sh
+++ extractPodman.sh
@@ -1,5 +1,5 @@
 # extract data from container
 rm -rf app
-container_id=$(podman create ghcr.io/opentestingapi/opentesting:1.40)
+container_id=$(podman create ghcr.io/opentestingapi/opentesting:1.41)
 podman cp $container_id:/app .
 podman rm $container_id
